01dd0e5631234b8a19413213503c5f2d1cd3cea8
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2024-01-18 16:14:41 +0100

    Add files via upload

--- c01/ex00/main.cpp
+++ c01/ex00/main.cpp
@@ -6,13 +6,19 @@
 /*   By: alpelliz <alpelliz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 12:47:30 by alpelliz          #+#    #+#             */
-/*   Updated: 2024/01/18 12:55:10 by alpelliz         ###   ########.fr       */
+/*   Updated: 2024/01/18 16:09:04 by alpelliz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie  *newZombie(std::string name);
+//void randomChump( std::string name );
+
 int main(void)
 {
-	
+	Zombie Zombie1("construtto");
+    Zombie* zombie2 = newZombie("Lucio");
+    Zombie1.announce();
+    zombie2->announce();
 }
--- c01/ex00/randomChump.cpp
+++ c01/ex00/randomChump.cpp
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alpelliz <alpelliz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 12:47:08 by alpelliz          #+#    #+#             */
-/*   Updated: 2024/01/18 16:09:21 by alpelliz         ###   ########.fr       */
+/*   Created: 2024/01/18 12:59:04 by alpelliz          #+#    #+#             */
+/*   Updated: 2024/01/18 16:13:22 by alpelliz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-#define ZOMBIE_HPP
+#include "Zombie.hpp"
 
-#include <iostream>
-
-class Zombie
+void randomChump( std::string name )
 {
-	private:
-		std::string _name;
-	public:
-		Zombie();
-		Zombie(std::string zombie_name);
-		~Zombie();
-		//void set_name(std::string str);
-		//std::string get_name(void);
-		void announce(void);
-		Zombie* newZombie(std::string name);
-		void randomChump( std::string name );
-};
-
-
-#endif
+	Zombie Zombie(name);
+	Zombie.announce();
+}
